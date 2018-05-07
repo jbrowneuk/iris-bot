5ccbc7d6c5993f286c8acf5e343f12af6396433e
Jason B <jaysunbee@gmail.com>
2018-05-07 15:45:19 +0100

    Refactor echo handler into its own personality construct

---
+++ src/personality/basic-intelligence.spec.ts
@@ -0,0 +1,27 @@
+import { Mock } from 'typemoq';
+import { BasicIntelligence } from './basic-intelligence';
+import { Message } from 'discord.js';
+
+describe('basic intelligence', () => {
+  it('should not handle an addressed message', (done: DoneFn) => {
+    const message = Mock.ofType<Message>();
+    message.setup(m => m.content).returns(() => 'anything');
+    const core = new BasicIntelligence();
+
+    core.onAddressed(message.object).then((result: string) => {
+      expect(result).toBe(null);
+      done();
+    });
+  });
+
+  it('should handle an ambient message with the echo command', (done: DoneFn) => {
+    const message = Mock.ofType<Message>();
+    message.setup(m => m.content).returns(() => '+echo');
+    const core = new BasicIntelligence();
+
+    core.onMessage(message.object).then((result: string) => {
+      expect(result).toBe('Echo!');
+      done();
+    });
+  });
+});
