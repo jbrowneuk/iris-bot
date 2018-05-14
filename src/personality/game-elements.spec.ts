144f3c459e5f77218d8cfe0c37255fc956192153
Jason B <jaysunbee@gmail.com>
2018-05-14 22:31:37 +0100

    Implement basic die rolling features

--- src/personality/game-elements.spec.ts
+++ src/personality/game-elements.spec.ts
@@ -26,6 +26,34 @@ describe('Game elements', () => {
     });
   });
 
+  it('should get heads when +flip command is issued and result greater than 0.5',
+    (done: DoneFn) => {
+      spyOn(Math, 'random').and.returnValue(0.6);
+
+      const message = Mock.ofType<Message>();
+      message.setup(m => m.content).returns(() => '+flip');
+      const core = new GameElements();
+
+      core.onMessage(message.object).then((result: string) => {
+        expect(result).toBe('heads');
+        done();
+      });
+    });
+
+  it('should get tails when +flip command is issued and result less than 0.5',
+    (done: DoneFn) => {
+      spyOn(Math, 'random').and.returnValue(0.4);
+
+      const message = Mock.ofType<Message>();
+      message.setup(m => m.content).returns(() => '+flip');
+      const core = new GameElements();
+
+      core.onMessage(message.object).then((result: string) => {
+        expect(result).toBe('tails');
+        done();
+      });
+    });
+
   it('should fnot handle a non-command', (done: DoneFn) => {
     const message = Mock.ofType<Message>();
     message.setup(m => m.content).returns(() => 'anything');
@@ -33,6 +61,76 @@ describe('Game elements', () => {
 
     core.onMessage(message.object).then((result: string) => {
       expect(result).toBeNull();
+      done();
+    });
+  });
+
+  it('should roll dice in the format <count>d<sides>', (done: DoneFn) => {
+    const message = Mock.ofType<Message>();
+    message.setup(m => m.content).returns(() => '+roll 4d6');
+    const core = new GameElements();
+
+    core.onMessage(message.object).then((result: string) => {
+      expect(result.startsWith('Rolling a 6-sided die 4 times:')).toBe(true);
+      done();
+    });
+  });
+
+  it('should roll a single die in the format d<sides>', (done: DoneFn) => {
+    const message = Mock.ofType<Message>();
+    message.setup(m => m.content).returns(() => '+roll d6');
+    const core = new GameElements();
+
+    core.onMessage(message.object).then((result: string) => {
+      expect(result.startsWith('Rolling a 6-sided die 1 times:')).toBe(true);
+      done();
+    });
+  });
+
+  it(
+    'should roll default number of dice when <count> is greater than a threshold',
+    (done: DoneFn) => {
+      const message = Mock.ofType<Message>();
+      message.setup(m => m.content).returns(() => '+roll 999d6');
+      const core = new GameElements();
+
+      core.onMessage(message.object).then((result: string) => {
+        expect(result.startsWith('Rolling a 6-sided die 1 times:')).toBe(true);
+        done();
+      });
+    });
+
+  it(
+    'should roll die with default number of sides when <sides> is greater than a threshold',
+    (done: DoneFn) => {
+      const message = Mock.ofType<Message>();
+      message.setup(m => m.content).returns(() => '+roll 1d999');
+      const core = new GameElements();
+
+      core.onMessage(message.object).then((result: string) => {
+        expect(result.startsWith('Rolling a 6-sided die 1 times:')).toBe(true);
+        done();
+      });
+    });
+
+  it('should not roll non-die strings', (done: DoneFn) => {
+    const message = Mock.ofType<Message>();
+    message.setup(m => m.content).returns(() => '+roll blobbly');
+    const core = new GameElements();
+
+    core.onMessage(message.object).then((result: string) => {
+      expect(result).toBeNull();
+      done();
+    });
+  });
+
+  it('should not roll non-die strings containing the letter d', (done: DoneFn) => {
+    const message = Mock.ofType<Message>();
+    message.setup(m => m.content).returns(() => '+roll badstr');
+    const core = new GameElements();
+
+    core.onMessage(message.object).then((result: string) => {
+      expect(result).toBe('Ignoring badstr');
       done();
     });
   });
--- src/personality/game-elements.ts
+++ src/personality/game-elements.ts
@@ -2,13 +2,22 @@ import * as discord from 'discord.js';
 import { Personality } from '../interfaces/personality';
 
 export class GameElements implements Personality {
-  public onAddressed(message: discord.Message, addressedMessage: string): Promise<string> {
+  public onAddressed(
+    message: discord.Message,
+    addressedMessage: string
+  ): Promise<string> {
     return Promise.resolve(null);
   }
 
   public onMessage(message: discord.Message): Promise<string> {
     return new Promise((resolve, reject) => {
-      const response = this.flipCoin(message);
+      let response = this.flipCoin(message);
+      if (response !== null) {
+        resolve(response);
+        return;
+      }
+
+      response = this.rollDice(message);
       if (response !== null) {
         resolve(response);
         return;
@@ -24,5 +33,77 @@ export class GameElements implements Personality {
     }
 
     return null;
+  }
+
+  private rollDice(message: discord.Message): string {
+    const rollCommand = '+roll';
+    if (
+      !message.content.startsWith(rollCommand) ||
+      message.content.charAt(rollCommand.length) !== ' '
+    ) {
+      return null;
+    }
+
+    const dice = this.parseDice(message.content.substr(rollCommand.length + 1));
+    if (dice.length === 0) {
+      return null;
+    }
+
+    return dice.join('\n');
+  }
+
+  private parseDice(input: string): string[] {
+    const potentialBits = input.toLowerCase().split(' ');
+    const bitsParsed = potentialBits.map((bit: string) =>
+      this.handleSingleDieRoll(bit)
+    );
+
+    return bitsParsed.filter((bit: string) => bit.length > 0);
+  }
+
+  private handleSingleDieRoll(rollInfo: string): string {
+    if (!rollInfo.includes('d')) {
+      return '';
+    }
+
+    const split = rollInfo.split('d', 2);
+    let numberDice = this.parseDieBit(split[0]);
+    let numberSides = this.parseDieBit(split[1]);
+
+    if (numberDice < 0 && numberSides < 0) {
+      return `Ignoring ${rollInfo}`;
+    }
+
+    if (numberDice < 1 || numberDice > 10) {
+      numberDice = 1;
+    }
+
+    if (numberSides < 4 || numberSides > 100) {
+      numberSides = 6;
+    }
+
+    return this.calculateDieRoll(numberDice, numberSides);
+  }
+
+  private parseDieBit(bit: string) {
+    if (!bit) {
+      return -1;
+    }
+
+    const parsed = Number.parseInt(bit, 10);
+    if (Number.isNaN(parsed)) {
+      return -1;
+    }
+
+    return parsed;
+  }
+
+  private calculateDieRoll(amount: number, sides: number): string {
+    const rolls: number[] = [];
+    for (let roll = 0; roll < amount; roll += 1) {
+      rolls.push(Math.ceil(Math.random() * sides));
+    }
+
+    return `Rolling a ${sides}-sided die ${amount} times: ${rolls.join(', ')}`;
   }
 }
