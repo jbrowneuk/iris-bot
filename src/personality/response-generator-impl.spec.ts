1e98f9f9e519aae8277660ff35f9a4275130e43c
Jason B <jaysunbee@gmail.com>
2018-07-14 21:53:53 +0100

    Refactor tests to handle new implementations

--- src/personality/response-generator-impl.spec.ts
+++ src/personality/response-generator-impl.spec.ts
@@ -1,16 +1,37 @@
+import { IMock, Mock, It, Times } from 'typemoq';
+import { Database } from '../interfaces/database';
+
 import { ResponseGeneratorImpl } from './response-generator-impl';
 
+const mockDbRows = [
+  { text: 'a' },
+  { text: 'b' },
+  { text: 'c' }
+];
+
 describe('response generator', () => {
+  let database: IMock<Database>;
+
+  beforeEach(() => {
+    database = Mock.ofType<Database>();
+    database
+      .setup(m => m.getRecordsFromCollection(It.isAnyString(), It.isAny()))
+      .returns((collection: string) => Promise.resolve(mockDbRows));
+  });
+
   it('should create', () => {
-    const gen = new ResponseGeneratorImpl();
+    const gen = new ResponseGeneratorImpl(database.object);
     expect(gen).toBeTruthy();
   });
 
-  it('should generate a response for a phrase', () => {
-    const gen = new ResponseGeneratorImpl();
+  it('should generate a response for a phrase', (done: DoneFn) => {
+    const gen = new ResponseGeneratorImpl(database.object);
 
-    const actualResponse = gen.generateResponse('phrase');
-
-    expect(actualResponse).toBe('');
+    gen.generateResponse('phrase').then((response: string) => {
+      // Verify that the result is equal to one of the texts in the rows
+      const mappedToMockRow = mockDbRows.find(x => x.text === response);
+      expect(mappedToMockRow).toBeTruthy();
+      done();
+    });
   });
 });
