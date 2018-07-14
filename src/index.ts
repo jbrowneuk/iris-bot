a78c784cf615efd2c5cceaae4bbebb59cff0c9b4
Jason B <jaysunbee@gmail.com>
2018-07-14 19:40:16 +0100

    Set up and tear down database layer on bot start and exit
    This is very much a hacky implementation currently; it would be much nicer for some kind of life cycle management system to handle this

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,13 @@ import { Engine } from './interfaces/engine';
 import { BasicIntelligence } from './personality/basic-intelligence';
 import { GameElements } from './personality/game-elements';
 import { HugBot } from './personality/hug-bot';
+import { Database } from './interfaces/database';
 
 const botEngine = container.get<Engine>(TYPES.Engine);
+const db = container.get<Database>(TYPES.Database);
+db.connect().then(() => {
+  console.log('Database connected');
+});
 
 // TODO: dep injection
 botEngine.addPersonality(new BasicIntelligence());
@@ -16,3 +21,8 @@ botEngine.addPersonality(new GameElements());
 botEngine.addPersonality(new HugBot());
 
 botEngine.run();
+
+process.on('beforeExit', () => {
+  console.log('Disconnecting database');
+  db.disconnect();
+});
