c0c57bc6b99fb466b15205347a163cb2e85dcca1
Jason B <jason@browne.me.uk>
2018-07-18 00:16:43 +0100

    Implement hugbot addressed mode

--- src/personality/hug-bot.spec.ts
+++ src/personality/hug-bot.spec.ts
@@ -17,26 +17,16 @@ describe('Hugbot', () => {
     message.setup(m => m.author).returns(() => author.object);
   });
 
-  it('should not handle an addressed message', (done: DoneFn) => {
-    message.setup(m => m.content).returns(() => 'anything');
-
-    const core = new HugBot();
-
-    core.onAddressed(message.object, 'anything').then((result: string) => {
-      expect(result).toBe(null);
-      done();
-    });
-  });
-
   it('should give an item to a user', () => {
-    const command = 'cmd';
+    const command = '!cmd';
     const item = 'spoopy';
-    message.setup(m => m.content).returns(() => `!${command} ${userToAddress}`);
+    const text = `${command} ${userToAddress}`;
+    message.setup(m => m.content).returns(() => text);
 
     const core = new HugBot();
 
     const untypedCore = core as any;
-    const response = untypedCore.response(message.object, command, item);
+    const response = untypedCore.response(message.object, text, command, item);
 
     expect(response).toBe(`Gives a ${item} to ${userToAddress} from <@${authorId}>`);
   });
@@ -52,12 +42,36 @@ describe('Hugbot', () => {
     });
   });
 
+  it('should give hug when asked to on an addressed message', (done: DoneFn) => {
+    const addressedMessage = `give a hug to ${userToAddress}`;
+    message.setup(m => m.content).returns(() => `@bot ${addressedMessage}`);
+
+    const core = new HugBot();
+
+    core.onAddressed(message.object, addressedMessage).then((result: string) => {
+      expect(result).toBe(`Gives a hug to ${userToAddress} from <@${authorId}>`);
+      done();
+    });
+  });
+
   it('should give a cake with emoji on !cake command', (done: DoneFn) => {
     message.setup(m => m.content).returns(() => `!cake ${userToAddress}`);
 
     const core = new HugBot();
 
     core.onMessage(message.object).then((result: string) => {
+      expect(result).toBe(`Gives a 🎂 to ${userToAddress} from <@${authorId}>`);
+      done();
+    });
+  });
+
+  it('should give cake when asked to on an addressed message', (done: DoneFn) => {
+    const addressedMessage = `give a cake to ${userToAddress}`;
+    message.setup(m => m.content).returns(() => `@bot ${addressedMessage}`);
+
+    const core = new HugBot();
+
+    core.onAddressed(message.object, addressedMessage).then((result: string) => {
       expect(result).toBe(`Gives a 🎂 to ${userToAddress} from <@${authorId}>`);
       done();
     });
