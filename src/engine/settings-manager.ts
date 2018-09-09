36410bf20db3b510ead9684b798f544d4576e468
Jason B <jaysunbee@gmail.com>
2018-09-09 17:00:14 +0100

    Cover settings manager with tests

--- src/engine/settings-manager.ts
+++ src/engine/settings-manager.ts
@@ -1,25 +1,26 @@
 import * as fs from 'fs';
-import { injectable } from 'inversify';
+import { injectable, optional } from 'inversify';
 
 import { Settings } from '../interfaces/settings';
 
-const path = './config.json';
+const defaultPath = './config.json';
 
 @injectable()
 export class SettingsManager implements Settings {
 
   private settingsData: { [key: string]: any };
 
-  constructor() {
+  constructor(@optional() path: string = defaultPath) {
     this.settingsData = {};
-    this.initialiseFromFile();
+
+    this.initialiseFromFile(path);
   }
 
   public getSettings(): { [key: string]: any } {
     return this.settingsData;
   }
 
-  private initialiseFromFile(): void {
+  private initialiseFromFile(path: string): void {
     const raw = fs.readFileSync(path, { encoding: 'utf8' });
     this.settingsData = JSON.parse(raw);
   }
