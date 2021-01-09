f0beffb7034b29037772fe3ebb2081787c80be3d
Jason B <jason@browne.me.uk>
2021-01-09 10:49:33 +0000

    Implement fetching of animal images from an API

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import { BasicIntelligence } from './personality/basic-intelligence';
 import { BlogRoll } from './personality/blog-roll';
 import { CallResponse } from './personality/call-response';
 import { DieRoll } from './personality/die-roll';
+import { FoxBot } from './personality/fox-bot';
 import { HugBot } from './personality/hug-bot';
 import { MoodControl } from './personality/mood-control';
 import { SimpleInteractions } from './personality/simple-interactions';
@@ -57,6 +58,7 @@ engine.addPersonality(new BlogRoll(dependencies));
 engine.addPersonality(new MoodControl(dependencies, moodEngine));
 engine.addPersonality(new SimpleInteractions(dependencies));
 engine.addPersonality(new CallResponse(dependencies));
+engine.addPersonality(new FoxBot(dependencies));
 
 // Start bot
 engine.initialise();
