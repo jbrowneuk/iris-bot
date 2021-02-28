c43087a7d3d09a74f29367f8a4e4a504e7ee12f9
Jason B <jason@browne.me.uk>
2021-02-28 17:31:01 +0000

    Implement fetching server status by command

--- src/personality/mc-server.spec.ts
+++ src/personality/mc-server.spec.ts
@@ -1,16 +1,76 @@
-import { McServer } from './mc-server';
+import { Guild, Message, MessageEmbed } from 'discord.js';
+import { IMock, Mock } from 'typemoq';
+
+import { McServer, ServerInformation, ServerResponse, statusCommand } from './mc-server';
+
+import util = require('minecraft-server-util');
+
+class TestableMcServer extends McServer {
+  public SetMockServer(discordId: string, info: ServerInformation): void {
+    this.servers.set(discordId, info);
+  }
+}
+
+const MOCK_GUILD_ID = 'mockguild';
+const MOCK_SERVER_INFO: ServerInformation = {
+  url: 'localhost',
+  channelId: null,
+  lastKnownOnline: false
+};
+
+const MOCK_RUNNING_STATUS: ServerResponse = {
+  version: '1.16.2',
+  onlinePlayers: 1,
+  maxPlayers: 5,
+  samplePlayers: [{ name: 'bob-bobertson' }]
+};
 
 describe('Minecraft server utilities', () => {
-  let personality: McServer;
+  let mockGuild: IMock<Guild>;
+  let personality: TestableMcServer;
 
   beforeEach(() => {
-    personality = new McServer();
+    mockGuild = Mock.ofType<Guild>();
+    mockGuild.setup((m) => m.id).returns(() => MOCK_GUILD_ID);
+
+    personality = new TestableMcServer();
   });
 
-  describe('Ambient messages', () => {
-    it('should resolve to null', (done) => {
-      personality.onMessage().then((response) => {
-        expect(response).toBeNull();
+  describe(`${statusCommand} messages`, () => {
+    it('should return embed with error if no server associated', (done) => {
+      const mockMessage = Mock.ofType<Message>();
+      mockMessage.setup((m) => m.content).returns(() => statusCommand);
+      mockMessage.setup((m) => m.guild).returns(() => mockGuild.object);
+
+      spyOn(util, 'status').and.callFake(() =>
+        Promise.resolve<any>(MOCK_RUNNING_STATUS)
+      );
+
+      personality.onMessage(mockMessage.object).then((response) => {
+        expect(response).toBeTruthy();
+        const embed = response as MessageEmbed;
+        expect(embed.title).toBeTruthy();
+        expect(embed.description).toContain('No Minecraft server associated');
+        done();
+      });
+    });
+
+    it('should return embed with server info if server associated', (done) => {
+      personality.SetMockServer(MOCK_GUILD_ID, MOCK_SERVER_INFO);
+
+      const mockMessage = Mock.ofType<Message>();
+      mockMessage.setup((m) => m.content).returns(() => statusCommand);
+      mockMessage.setup((m) => m.guild).returns(() => mockGuild.object);
+
+      spyOn(util, 'status').and.callFake(() =>
+        Promise.resolve<any>(MOCK_RUNNING_STATUS)
+      );
+
+      personality.onMessage(mockMessage.object).then((response) => {
+        expect(response).toBeTruthy();
+        const embed = response as MessageEmbed;
+        expect(embed.title).toBeTruthy();
+        expect(embed.description).toContain('Your server is');
         done();
       });
     });
--- src/personality/mc-server.ts
+++ src/personality/mc-server.ts
@@ -1,16 +1,113 @@
+import { Message, MessageEmbed } from 'discord.js';
+
 import { Personality } from '../interfaces/personality';
 import { MessageType } from '../types';
 
+import util = require('minecraft-server-util');
+
+export interface ServerInformation {
+  url: string;
+  channelId: string;
+  lastKnownOnline: boolean;
+}
+
+export interface ServerPlayer {
+  name: string;
+}
+
+export interface ServerResponse {
+  version: string;
+  onlinePlayers: number;
+  maxPlayers: number;
+  samplePlayers: ServerPlayer[];
+}
+
+// Personality constants
+const embedTitle = 'Server info';
+const embedErrorColor = 0xff8000;
+const embedSuccessColor = 0x0080ff;
+
+// Commands
+export const statusCommand = '+MCSTATUS';
+
 export class McServer implements Personality {
+  protected servers: Map<string, ServerInformation>;
+  protected timerInterval: number | NodeJS.Timer;
+
+  constructor() {
+    this.servers = new Map<string, ServerInformation>();
+  }
+
   public onAddressed(): Promise<MessageType> {
     return Promise.resolve(null);
   }
 
-  public onMessage(): Promise<MessageType> {
+  public onMessage(message: Message): Promise<MessageType> {
+    const messageText = message.content.toUpperCase();
+    const discordServerId = message.guild.id;
+    const embed = new MessageEmbed();
+    embed.setTitle(embedTitle);
+
+    if (messageText === statusCommand) {
+      if (!this.servers.has(discordServerId)) {
+        embed.setDescription(
+          'No Minecraft server associated with this Discord'
+        );
+        embed.setColor(embedErrorColor);
+        return Promise.resolve(embed);
+      }
+
+      const minecraftServerUrl = this.servers.get(discordServerId).url;
+      return this.getServerStatus(minecraftServerUrl)
+        .then((response) => {
+          const serverStatus = this.generateServerEmbed(response);
+          return serverStatus;
+        })
+        .catch(() => {
+          const serverStatus = this.generateServerEmbed(null);
+          return serverStatus;
+        });
+    }
+
     return Promise.resolve(null);
   }
 
   onHelp(): Promise<MessageType> {
     return Promise.resolve(null);
+  }
+
+  private getServerStatus(url: string): Promise<ServerResponse> {
+    return util
+      .status(url)
+      .then((response: ServerResponse) => {
+        // Handle Aternos servers
+        if (response.version.match(/\d+\.\d+\.\d+/g) === null) {
+          return null;
+        }
+
+        return response;
+      })
+      .catch(
+        (error: Error): ServerResponse => {
+          console.error('Server unreachable:', error);
+          return null;
+        }
+      );
+  }
+
+  private generateServerEmbed(status: ServerResponse): MessageEmbed {
+    const isOnline = status && status !== null;
+    const embed = new MessageEmbed();
+    embed.setTitle(embedTitle);
+    embed.setColor(isOnline ? embedSuccessColor : embedErrorColor);
+    embed.setDescription(`Your server is ${isOnline ? 'online' : 'offline'}`);
+
+    if (isOnline) {
+      const players = status.samplePlayers.map((p) => p.name);
+      const playerCount = `${status.onlinePlayers}/${status.maxPlayers} players`;
+      embed.addField('Status', `${playerCount}:\n${players.join('\n')}`);
+    }
+
+    return embed;
   }
 }
