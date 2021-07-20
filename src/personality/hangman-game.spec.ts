fafcf8e486a5499c56e2780cbb49163976855699
Jason B <jason@browne.me.uk>
2021-07-20 15:46:52 +0100

    Implement fetching of word from API

--- src/personality/hangman-game.spec.ts
+++ src/personality/hangman-game.spec.ts
@@ -1,13 +1,45 @@
+import { Message } from 'discord.js';
+import * as nodeFetch from 'node-fetch';
+import { Mock } from 'typemoq';
+
 import { HangmanGame } from './hangman-game';
 
 describe('Hangman game', () => {
+  let fetchSpy: jasmine.Spy;
   let personality: HangmanGame;
 
   beforeEach(() => {
-    personality = new HangmanGame();
+    personality = new HangmanGame(null);
+    fetchSpy = spyOn(nodeFetch, 'default');
   });
 
   it('should create', () => {
     expect(personality).toBeTruthy();
+  });
+
+  describe('help functionality', () => {
+    it('should respond with help message', (done) => {
+      personality.onHelp().then((response) => {
+        expect(response).toBeTruthy();
+        done();
+      });
+    });
+  });
+
+  fit('should fetch random word on +hangman-test command', (done) => {
+    const mockWord = { word: 'word' };
+    const mockFetchResponse = {
+      ok: true,
+      json: () => Promise.resolve(mockWord)
+    };
+    fetchSpy.and.returnValue(Promise.resolve(mockFetchResponse));
+
+    const mockMessage = Mock.ofType<Message>();
+    mockMessage.setup((s) => s.content).returns(() => '+hangman-test');
+
+    personality.onMessage(mockMessage.object).then((response) => {
+      expect(response).toBe(mockWord.word);
+      done();
+    });
   });
 });
--- src/personality/hangman-game.ts
+++ src/personality/hangman-game.ts
@@ -1,18 +1,44 @@
 import { Message } from 'discord.js';
+import * as nodeFetch from 'node-fetch';
 
+import { DependencyContainer } from '../interfaces/dependency-container';
 import { Personality } from '../interfaces/personality';
 import { MessageType } from '../types';
 
+const apiUrl = 'https://jbrowne.io/api/words/';
+
 export class HangmanGame implements Personality {
+  constructor(private dependencies: DependencyContainer) {}
+
   onAddressed(message: Message, messageText: string): Promise<MessageType> {
     return Promise.resolve(null);
   }
 
   onMessage(message: Message): Promise<MessageType> {
+    if (message.content.startsWith('+hangman-test')) {
+      return nodeFetch
+        .default(apiUrl)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Unable to fetch API');
+          }
+
+          return response.json();
+        })
+        .then((rawData) => this.handleWordResponse(rawData))
+        .catch((e) => {
+          this.dependencies.logger.error(e);
+          return 'My internet is not working right now.';
+        });
+    }
     return Promise.resolve(null);
   }
 
-  onHelp?(): Promise<MessageType> {
-    return Promise.resolve('');
+  onHelp(): Promise<MessageType> {
+    return Promise.resolve('no help');
+  }
+
+  private handleWordResponse(wordData: any): MessageType {
+    return wordData && wordData.word ? wordData.word : 'no words';
   }
 }
