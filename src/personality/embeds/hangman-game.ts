17e44e0c51218f98e99a5bc39b227c8c262f5268
Jason B <jason@browne.me.uk>
2021-11-30 10:38:47 +0000

    Set default colour for hangman game embeds

--- src/personality/embeds/hangman-game.ts
+++ src/personality/embeds/hangman-game.ts
@@ -1,41 +1,25 @@
 import { MessageEmbed } from 'discord.js';
 
-import {
-  guessCommand,
-  prefix,
-  startCommand,
-  statsCommand,
-  summaryCommand
-} from '../constants/hangman-game';
+import { guessCommand, prefix, startCommand, statsCommand, summaryCommand } from '../constants/hangman-game';
 import { GameData } from '../interfaces/hangman-game';
 
-const embedTitle = 'Hangman';
+export const embedTitle = 'Hangman';
+export const embedColor = '#0080ff';
 
 function generateBaseEmbed(): MessageEmbed {
   const embed = new MessageEmbed();
   embed.setTitle(embedTitle);
+  embed.setColor(embedColor);
   return embed;
 }
 
 export function generateHelpEmbed(): MessageEmbed {
   const embed = generateBaseEmbed();
   embed.setDescription('Plays a game of hangman using words from web sources.');
-  embed.addField(
-    'Game summary',
-    `Use \`${prefix} ${summaryCommand}\` to see a summary of the game.`
-  );
-  embed.addField(
-    'Starting a game',
-    `Use \`${prefix} ${startCommand}\` to start a game.`
-  );
-  embed.addField(
-    'Making guesses',
-    `Use \`${prefix} ${guessCommand} <your guess>\` to guess a letter or word.`
-  );
-  embed.addField(
-    'Viewing stats',
-    `Use \`${prefix} ${statsCommand}\` to see the current server stats.`
-  );
+  embed.addField('Game summary', `Use \`${prefix} ${summaryCommand}\` to see a summary of the game.`);
+  embed.addField('Starting a game', `Use \`${prefix} ${startCommand}\` to start a game.`);
+  embed.addField('Making guesses', `Use \`${prefix} ${guessCommand} <your guess>\` to guess a letter or word.`);
+  embed.addField('Viewing stats', `Use \`${prefix} ${statsCommand}\` to see the current server stats.`);
 
   return embed;
 }
@@ -46,16 +30,11 @@ export function generateGameEmbed(gameData: GameData): MessageEmbed {
   const countDisplay = `(${gameData.currentDisplay.length} letters)`;
   const chanceDisplay = `${gameData.livesRemaining} chances left`;
   embed.setDescription(`${letterDisplay} ${countDisplay}\n${chanceDisplay}`);
-  const lettersSummary =
-    gameData.wrongLetters.length > 0 ? gameData.wrongLetters.join() : '*none*';
+  const lettersSummary = gameData.wrongLetters.length > 0 ? gameData.wrongLetters.join() : '*none*';
 
-  const wordsSummary =
-    gameData.wrongWords.length > 0 ? gameData.wrongWords.join() : '*none*';
+  const wordsSummary = gameData.wrongWords.length > 0 ? gameData.wrongWords.join() : '*none*';
 
-  embed.addField(
-    'Wrong guesses',
-    `Letters: ${lettersSummary}\nWords: ${wordsSummary}`
-  );
+  embed.addField('Wrong guesses', `Letters: ${lettersSummary}\nWords: ${wordsSummary}`);
 
   return embed;
 }
