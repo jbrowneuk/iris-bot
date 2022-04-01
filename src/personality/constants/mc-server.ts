0fdd9e720e194d812464a77606209ce18ebdd955
Jason B <jason@browne.me.uk>
2022-04-01 21:37:19 +0100

    Update MC server commands to use command prefix

--- src/personality/constants/mc-server.ts
+++ src/personality/constants/mc-server.ts
@@ -1,7 +1,9 @@
+import { COMMAND_PREFIX } from '../../constants/personality-constants';
+
 // Commands
-export const statusCommand = '+MCSTATUS';
-export const setCommand = '+MCSET';
-export const announceCommand = '+MCANNOUNCE';
+export const statusCommand = COMMAND_PREFIX + 'MCSTATUS';
+export const setCommand = COMMAND_PREFIX + 'MCSET';
+export const announceCommand = COMMAND_PREFIX + 'MCANNOUNCE';
 
 // Responses
 export const noAssociationCopy = 'No Minecraft server associated with this Discord';
